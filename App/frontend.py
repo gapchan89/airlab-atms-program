22a941ac95abf2e61f8e9001513f48d19054a020
Adrian <gapchan89@gmail.com>
2023-10-01 22:27:08 +0800

    Updated frontend.yaml file to log API call requests Updated deployment file to use imagePullPolicy Updated github local action workflow for MacOS

--- App/frontend.py
+++ App/frontend.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # API call to get SID
 @app.route('/api/sid/<string:airport_code>', methods=['GET'])
 def get_sid(airport_code):
-    
+    print("Received request: /api/sid/" + airport_code)
     response = backend.calculate_sid(airport_code)
     
     return jsonify(response)
@@ -14,6 +14,7 @@ def get_sid(airport_code):
 # API call to get Stars
 @app.route('/api/stars/<string:airport_code>', methods=['GET'])
 def get_stars(airport_code):
+    print("Received request: /api/stars/" + airport_code)
     response = backend.calculate_stars(airport_code)
     
     return jsonify(response)
